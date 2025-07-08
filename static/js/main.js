9d2965f44f2fbead92f1033ca88714d2fb58f32e
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-07-07 21:48:39 -0300

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
+class Tarefa {
+    constructor(titulo, descricao) {
+        this.titulo = titulo
+        this.descricao = descricao
+    }
+}
+
 // Função para a criação de tarefa
 const criarTarefa = function () {
 
@@ -35,7 +42,7 @@ const criarTarefa = function () {
             if (result.isConfirmed) {
 
                 // lógica para criar a tarefa mesmo sem descrição
-                adicionarTarefa()
+                adicionarTarefa(document.getElementById('titulo').value)
 
                 // Limpando input de colocar título e input de colocar a descrição
                 document.getElementById('titulo').value = ""
@@ -60,13 +67,54 @@ const criarTarefa = function () {
     else {
 
         // Adicionar tarefa
-        adicionarTarefa()
+        adicionarTarefa(document.getElementById('titulo').value, document.getElementById('descricao').value)
     }
 
 }
 
 // Função para adicionar tarefa
 const adicionarTarefa = function (titulo, descricao = "") {
+
+    // Criando novo objeto "Tarefa"
+    let tarefa = new Tarefa(titulo, descricao)
+
+    // Guardando nova tarefa em string no localStorage
+    localStorage.setItem(titulo, JSON.stringify(tarefa));
+
+    // Variaveis da tarefa salva
+    const tarefaSalvaObject = JSON.parse(localStorage.getItem(titulo))
+    const tarefaSalvaString = localStorage.getItem(titulo)
+
+    // Variavel para separar título e descrição
+    const tituloTarefa = function () { return tarefaSalvaObject.titulo }
+    const descricaoTarefa = function () { return tarefaSalvaObject.descricao }
+
+
+
+    // Variaveis para exibir a tarefa no front-end
+    const lista = document.querySelector('ul')
+    const li = document.createElement('li')
+    const divTarefa = document.createElement('div')
+    const divTitulo = document.createElement('div')
+    const checkbox = document.createElement('input')
+    const spanCheckbox = document.createElement('span')
+    const label = document.createElement('label')
+
+    checkbox.type = 'checkbox';
+    spanCheckbox.classList.add('checkbox')
+
+    label.classList.add('checkbox-wrapper')
+
+    divTarefa.classList.add('tarefa')
+
+    divTitulo.textContent = tituloTarefa()
+    lista.appendChild(li)
+
+    li.appendChild(divTarefa)
+    divTarefa.appendChild(divTitulo)
+    divTarefa.appendChild(label)
+    label.appendChild(checkbox)
+    label.appendChild(spanCheckbox)
 
 }
 
